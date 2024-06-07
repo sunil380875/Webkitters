a455efc7b91a994f0e2404b3942e405886f73a58
sunil380875 <sunil380mishra@gmail.com>
2024-06-07 22:37:32 +0530

    helper function added

---
+++ src/logic/pagination.helper.ts
@@ -0,0 +1,63 @@
+const { Model } = require("mongoose");
+
+export const aggregationData = async ({
+  model,
+  args,
+  position = args?.length,
+  sort,
+  per_page,
+  pageNo,
+  isTotalData,
+}: any) => {
+  try {
+    sort &&
+      args.splice(position, 0, {
+        $sort: sort,
+      });
+
+    if (
+      typeof per_page === "number" &&
+      typeof pageNo === "number" &&
+      per_page > -1 &&
+      pageNo > -1
+    ) {
+      const perPage = per_page;
+      const skip = +perPage * pageNo;
+      const totalCount = isTotalData
+        ? await model.aggregate([
+            ...args,
+            {
+              $count: "totalCount",
+            },
+          ])
+        : undefined;
+      args.splice(position + 1, 0, {
+        $skip: skip,
+      });
+      args.splice(position + 2, 0, {
+        $limit: per_page + 1,
+      });
+      const dataGet = await model.aggregate(args);
+      const haveNextPage = Boolean(dataGet.length === Number(perPage) + 1);
+      if (haveNextPage) {
+        dataGet.pop();
+      }
+
+      return {
+        data: dataGet,
+        haveNextPage,
+        pageNo: isTotalData ? pageNo : undefined,
+        perPage: isTotalData ? per_page : undefined,
+        totalCount: totalCount?.[0]?.totalCount,
+      };
+    } else {
+      const dataGet = await model.aggregate(args);
+      return {
+        data: dataGet,
+        haveNextPage: false,
+      };
+    }
+  } catch (error) {
+    throw error;
+  }
+};
